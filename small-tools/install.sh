3999e75606a19150ac32bfe3c3a67221621d9c6d
cyc <ycpss91255@gmail.com>
2024-02-20 16:02:43 +0800

    del: docker-ctop

--- small-tools/install.sh
+++ small-tools/install.sh
@@ -10,19 +10,23 @@ sudo apt update && \
 
 # Install small tools dependencies
 sudo apt install -y --no-install-recommends \
+    ca-certificates \
     curl \
     git \
+    gnupg \
+    lsb-release \
     python3 \
     python3-pip \
+    software-properties-common \
     && \
 pip install -U pip && \
 
 # Install required packages for 'monitoring tools'
+sudo apt-get update && \
 sudo apt install -y --no-install-recommends \
     bat \
     bashtop \
     bmon \
-    docker-ctop \
     htop \
     iftop \
     iotop \
@@ -31,9 +35,6 @@ sudo apt install -y --no-install-recommends \
     && \
 pip install bpytop \
     git+https://github.com/XuehaiPan/nvitop.git#egg=nvitop && \
-# echo Success or failure message
-echo -e "\033[1;37;42mMonitoring tools install successfully.\033[0m" || \
-echo -e "\033[1;37;41mSmall tools Install failed.\033[0m"
 
 # Install required packages for 'other tools'
 sudo apt install -y --no-install-recommends \
@@ -49,7 +50,8 @@ sudo apt install -y --no-install-recommends \
 
 # Install required packages for 'ranger'
 sudo apt install -y --no-install-recommends \
-    ranger && \
+    ranger \
+    && \
 git clone https://github.com/alexanderjeurissen/ranger_devicons \
         /home/"${USER_NAME}"/.config/ranger/plugins/ranger_devicons && \
 

3d8f0d251cfed38f5971bf23eaf9c792f364994c
cyc <ycpss91255@gmail.com>
2024-02-22 14:26:31 +0800

    add: tmux install and config

--- small-tools/install.sh
+++ small-tools/install.sh
@@ -2,8 +2,8 @@
 
 # ${1}: USER NAME. Use the provided username, or default to the current user ($USER).
 
-# SCRIPT_PATH=$(dirname "$(readlink -f "${0}")")
 USER_NAME=${1:-"$USER"}
+SCRIPT_PATH=$(dirname "$(readlink -f "${0}")")
 
 # Update the package lists
 sudo apt update && \
@@ -46,6 +46,7 @@ sudo apt install -y --no-install-recommends \
     ranger \
     tig \
     tldr \
+    tmux \
     tree \
     ssh \
     sshfs \
@@ -59,12 +60,18 @@ sudo ln -s $(which batcat) /usr/bin/bat && \
 # tldr update
 sudo tldr --update && \
 
-# delete old ranger_devicons, avoid problems
-rm -rf /home/"${USER_NAME}"/.config/ranger/plugins/ranger_devicons && \
-# Install ranger plugins 'ranger_devicons'
-git clone https://github.com/alexanderjeurissen/ranger_devicons \
-        /home/"${USER_NAME}"/.config/ranger/plugins/ranger_devicons && \
+# delete old tmux plugin manager, avoid problems
+rm -rf /home/"${USER_NAME}"/.tmux/plugins/tpm && \
+# install new plugin manager
+git clone --depth 1 \
+    https://github.com/tmux-plugins/tpm \
+    /home/"${USER_NAME}"/.tmux/plugins/tpm && \
+# copy tmux configuration file
+cp "${SCRIPT_PATH}"/config/tmux.conf /home/"${USER_NAME}"/.tmux.conf && \
+/home/"${USER_NAME}"/.tmux/plugins/tpm/scripts/install_plugins.sh && \
 
+# copy ssh config template to ~/.ssh/config
+cp "${SCRIPT_PATH}"/config/ssh_config /home/"${USER_NAME}"/.ssh/config && \
 # enable X11Forwarding
 sudo sed -i 's/#\s*\(ForwardX11 yes\)/\1/' '/etc/ssh/ssh_config' && \
 sudo sed -i -e 's/#\s*\(AllowTcpForwarding yes\)/\1/' \
@@ -74,6 +81,13 @@ sudo sed -i -e 's/#\s*\(AllowTcpForwarding yes\)/\1/' \
            '/etc/ssh/sshd_config' && \
 sudo systemctl enable ssh && \
 sudo systemctl restart ssh && \
+
+# delete old ranger_devicons, avoid problems
+rm -rf /home/"${USER_NAME}"/.config/ranger/plugins/ranger_devicons && \
+# Install ranger plugins 'ranger_devicons'
+git clone --depth 1 \
+    https://github.com/alexanderjeurissen/ranger_devicons \
+    /home/"${USER_NAME}"/.config/ranger/plugins/ranger_devicons && \
 
 # print Success or failure message
 printf "\033[1;37;42mSmall tools install successfully.\033[0m\n" || \

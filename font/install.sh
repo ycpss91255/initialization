a825cbfc8a6e0a6880c6eba1f3819b6ca6595c43
cyc <ycpss91255@gmail.com>
2024-02-22 10:45:50 +0800

    temp: use url download fonts, but there are some problems

--- font/install.sh
+++ font/install.sh
@@ -23,3 +23,21 @@ sudo fc-cache --force --verbose && \
 # print Success or failure message
 printf "\033[1;37;42mFonts install successfully.\033[0m" || \
 printf "\033[1;37;41mFonts Install failed.\033[0m"
+
+
+# BUG: not download noto sans font...
+# FIRACODE_VERSION=$(curl -s "https://api.github.com/repos/tonsky/FiraCode/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+
+# NERDFONTS_VERSION=$(curl -s "https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
+
+# curl -Lo ./fonts/FiraCode.zip "https://github.com/tonsky/FiraCode/releases/dwnload/${FIRACODE_VERSION}/Fira_Code_${FIRACODE_VERSION}.zip"
+
+
+# FONT_NF=('FiraCode' 'Meslo' 'SourceCodePro')
+
+# mkdir -p ./fonts
+
+# for font in ${FONT_NF[@]}; do
+#     curl -Lo "./fonts/${font}.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/${NERDFONTS_VERSION}/${font}.zip"
+# done
+

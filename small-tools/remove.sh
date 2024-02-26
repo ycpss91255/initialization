40208913a0e990fc2d78a1f9d2fb4833a4ad48b0
cyc <ycpss91255@gmail.com>
2024-02-27 00:04:49 +0800

    add: remove fzf script

--- small-tools/remove.sh
+++ small-tools/remove.sh
@@ -30,12 +30,12 @@ fish -c "fisher remove \
     danhper/fish-ssh-agent \
     jorgebucaran/nvm.fish \
     && \
+    /home/"${USER_NAME}"/.fzf/uninstall && \
     set -e fish_user_paths" && \
 rm -rf /home/"${USER_NAME}"/.config/fish && \
 sudo add-apt-repository --remove -y ppa:fish-shell/release-3 && \
 
 # delete 'fzf' configuration file
-fish -c "/home/"${USER_NAME}"/.fzf/uninstall" && \
 rm -rf /home/"${USER_NAME}"/.fzf && \
 
 # delete tldr folder

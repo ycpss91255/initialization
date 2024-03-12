c2becd70d0dfd753528737bf5f35abe7b411824a
cyc <ycpss91255@gmail.com>
2024-03-13 03:39:57 +0800

    adjust: fish config and color scheme

--- small-tools/config/fish/config.fish
+++ small-tools/config/fish/config.fish
@@ -1,15 +1,24 @@
 if status is-interactive
     # WSL
-    # if not pgrep -x "sshd" > /dev/null
-       # sudo service ssh --full-restart > /dev/null
-    # end
+    if not pgrep -x "sshd" > /dev/null
+       sudo service ssh --full-restart > /dev/null
+    end
 
     # Commands to run in interactive sessions can go here
 end
 
+
 # Use Neovim
 if dpkg -l | grep -q "snap" && snap list | grep -q "nvim"
     alias vim="nvim"
+    alias view="nvim -R"
+    export EDITOR="nvim"
+else
+    export EDITOR="vim"
+end
+
+if dpkg -l | grep -q "xdg-utils"
+    alias xopen="xdg-open"
 end
 
 # use node js version 18, nvim lps dep
@@ -17,6 +26,10 @@ if type -q "fisher" && fisher list | grep -q "nvm"
     nvm use 18 >/dev/null 2>&1
 end
 
+# use node js version 18, nvim lps dep
+if type -q "fisher" && fisher list | grep -q "zoxide"
+    zoxide init fish | source
+end
 # TODO: add ssh-agent support and ssh-add key support
 # if type -q "fisher" && Fisher list | grep -q "ssh-agent"
 #     ssh-agent fish -c "ssh-add"
@@ -25,9 +38,6 @@ end
 
 # alias and short function
 ## Edit fish user key bindings
-alias ll="ls -alhF"
-alias la="ls -A"
-alias l="ls -CF"
 
 function ehk; vim ~/.config/fish/functions/fish_user_key_bindings.fish; end
 function shk; source ~/.config/fish/functions/fish_user_key_bindings.fish; end

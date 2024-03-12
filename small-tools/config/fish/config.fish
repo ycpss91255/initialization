fc9c915425588ab7f54c239d60408107ab02fd17
cyc <ycpss91255@gmail.com>
2024-03-13 05:07:13 +0800

    adjust: done ssh-agent and fix config.fish bug

--- small-tools/config/fish/config.fish
+++ small-tools/config/fish/config.fish
@@ -4,46 +4,83 @@ if status is-interactive
        sudo service ssh --full-restart > /dev/null
     end
 
-    # Commands to run in interactive sessions can go here
+    # Use Neovim
+    if dpkg --get-selections | grep -q "snap[[:space:]]*install"
+        if snap list | grep -q "nvim"
+            alias vim="nvim"
+            alias view="nvim -R"
+            set -x EDITOR "nvim"
+        end
+    else
+        set -x EDITOR "vim"
+    end
+
+    if dpkg --get-selections | grep -q "xdg-utils[[:space:]]*install"
+        alias xopen="xdg-open"
+    end
+
+    # use node js version 18, nvim lps dep
+    if type -q "fisher"
+        if fisher list | grep -q "nvm"
+            nvm use 18 >/dev/null 2>&1
+        end
+    end
+
+    # use node js version 18, nvim lps dep
+    if type -q "fisher"
+        if fisher list | grep -q "zoxide"
+            zoxide init fish | source
+        end
+    end
+
+    # SSH agent
+    setenv SSH_ENV $HOME/.ssh/environment
+
+    function start_agent
+        echo "Initializing new SSH agent ..."
+        ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+        echo "succeeded"
+        chmod 600 $SSH_ENV
+        . $SSH_ENV > /dev/null
+        ssh-add
+    end
+
+    function test_identities
+        ssh-add -l | grep "The agent has no identities" > /dev/null
+        if [ $status -eq 0 ]
+            ssh-add
+            if [ $status -eq 2 ]
+                start_agent
+            end
+        end
+    end
+
+    if [ -n "$SSH_AGENT_PID" ]
+        ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+        if [ $status -eq 0 ]
+            test_identities
+        end
+    else
+        if [ -f $SSH_ENV ]
+            . $SSH_ENV > /dev/null
+        end
+        ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+        if [ $status -eq 0 ]
+            test_identities
+        else
+            start_agent
+        end
+    end
+
+    # alias and short function
+    ## Edit fish user key bindings
+
+    function ehk; vim ~/.config/fish/functions/fish_user_key_bindings.fish; end
+    function shk; source ~/.config/fish/functions/fish_user_key_bindings.fish; end
+    ## fish config
+    function efc; vim ~/.config/fish/config.fish; end
+    function sfc; source ~/.config/fish/config.fish && echo "source user config.fish"; end
+    ## tmux config
+    function etc; vim ~/.tmux.conf; end
+    function stc; tmux source-file ~/.tmux.conf; end
 end
-
-
-# Use Neovim
-if dpkg -l | grep -q "snap" && snap list | grep -q "nvim"
-    alias vim="nvim"
-    alias view="nvim -R"
-    export EDITOR="nvim"
-else
-    export EDITOR="vim"
-end
-
-if dpkg -l | grep -q "xdg-utils"
-    alias xopen="xdg-open"
-end
-
-# use node js version 18, nvim lps dep
-if type -q "fisher" && fisher list | grep -q "nvm"
-    nvm use 18 >/dev/null 2>&1
-end
-
-# use node js version 18, nvim lps dep
-if type -q "fisher" && fisher list | grep -q "zoxide"
-    zoxide init fish | source
-end
-# TODO: add ssh-agent support and ssh-add key support
-# if type -q "fisher" && Fisher list | grep -q "ssh-agent"
-#     ssh-agent fish -c "ssh-add"
-#     ssh-agent fish -c "ssh-add ~/.ssh/id_rsa"
-# end
-
-# alias and short function
-## Edit fish user key bindings
-
-function ehk; vim ~/.config/fish/functions/fish_user_key_bindings.fish; end
-function shk; source ~/.config/fish/functions/fish_user_key_bindings.fish; end
-## fish config
-function efc; vim ~/.config/fish/config.fish; end
-function sfc; source ~/.config/fish/config.fish && echo "source user config.fish"; end
-## tmux config
-function etc; vim ~/.tmux.conf; end
-function stc; tmux source-file ~/.tmux.conf; end

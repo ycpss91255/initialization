79eb315d73fb057713f70caf46446b17ba8a81a3
cyc <ycpss91255@gmail.com>
2024-06-16 22:17:34 +0800

    add: fish function

--- small-tools/config/fish/functions/docker-build-run.fish
+++ small-tools/config/fish/functions/docker-build-run.fish
@@ -1,9 +1,24 @@
 function docker-build-run --description "Build and run the docker container"
-    set script_dir (if test (count $argv) -gt 0; echo $argv[1]; else; pwd; end)
+    if test (count $argv) -gt 0
+        set SCRIPT_DIR $argv[1]
+    else
+        set SCRIPT_DIR (pwd)
+    end
 
-    docker system prune -f
+    if not docker system prune -f
+        echo "Docker system prune failed."
+        return 1
+    end
 
-    $script_dir/build.sh &&
-    clear &&
-    $script_dir/run.sh
+    if not $SCRIPT_DIR/build.sh
+        echo "Build failed."
+        return 1
+    end
+
+    clear
+
+    if not $SCRIPT_DIR/run.sh
+        echo "Run failed."
+        return 1
+    end
 end

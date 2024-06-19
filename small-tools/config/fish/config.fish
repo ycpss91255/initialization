0c51f308504def884b25a1cf63fcd037d8e27777
cyc <ycpss91255@gmail.com>
2024-06-19 12:40:09 +0800

    adjust: fish config

--- small-tools/config/fish/config.fish
+++ small-tools/config/fish/config.fish
@@ -33,19 +33,20 @@ if status is-interactive
         end
     end
 
+
     # SSH agent
     setenv SSH_ENV $HOME/.ssh/environment
 
-    function start_agent
+    function start_agent                                                                                                                                                                    
         echo "Initializing new SSH agent ..."
         ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
         echo "succeeded"
-        chmod 600 $SSH_ENV
+        chmod 600 $SSH_ENV 
         . $SSH_ENV > /dev/null
         ssh-add
     end
 
-    function test_identities
+    function test_identities                                                                                                                                                                
         ssh-add -l | grep "The agent has no identities" > /dev/null
         if [ $status -eq 0 ]
             ssh-add
@@ -55,27 +56,21 @@ if status is-interactive
         end
     end
 
-    if [ -n "$SSH_AGENT_PID" ]
+    if [ -n "$SSH_AGENT_PID" ] 
         ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
         if [ $status -eq 0 ]
             test_identities
-        end
+        end  
     else
         if [ -f $SSH_ENV ]
             . $SSH_ENV > /dev/null
-        end
+        end  
         ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
         if [ $status -eq 0 ]
             test_identities
-        else
+        else 
             start_agent
-        end
+        end  
     end
-
-    # alias and short function
-    ## Edit fish user key bindings
-    function update_upgrade
-        sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean
-    end
-
 end
+

89017fe95bb8133e9ce8b6ca289eff687f9501bd
cyc <ycpss91255@gmail.com>
2024-12-11 21:36:22 +0800

    adjust: tmux.config and fix fish stc function

--- small-tools/config/fish/config.fish
+++ small-tools/config/fish/config.fish
@@ -41,7 +41,7 @@ if status is-interactive
         echo "Initializing new SSH agent ..."
         ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
         echo "succeeded"
-        chmod 600 $SSH_ENV 
+        chmod 600 $SSH_ENV
         . $SSH_ENV > /dev/null
         ssh-add
     end

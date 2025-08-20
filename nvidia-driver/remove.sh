f356da7dd275e281d06ae8c51c19ede697b6adf1
cyc <ycpss91255@gmail.com>
2025-08-20 16:09:14 +0800

    review: install and remove script

--- nvidia-driver/remove.sh
+++ nvidia-driver/remove.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-# ${1}: USER NAME. Use the provided username, or default to the current user ($USER).
-
-# SCRIPT_PATH=$(dirname "$(readlink -f "${0}")")
-# USER_NAME=${1:-"$USER"}
-
 INSTALLED_NVIDIA_DRIVER=$(dpkg --get-selections | grep -oP '^nvidia-driver-\d+' | head -1)
 
 pip uninstall -y nvitop
 
 if [ -n "${INSTALLED_NVIDIA_DRIVER}" ]; then
-    sudo apt purge -y "${INSTALLED_NVIDIA_DRIVER}" && \
+    sudo apt purge -y "${INSTALLED_NVIDIA_DRIVER}"
 
     # print Success or failure message
-    printf "\033[1;37;42mNVIDIA driver purge successfully.\033[0m\n" || \
-    printf "\033[1;37;41mNVIDIA driver purge failed.\033[0m\n"
+    printf "\033[1;37;42mNVIDIA driver purge successfully.\033[0m\n"
 fi
 

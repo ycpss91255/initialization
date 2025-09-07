70b80932d0ebeff2e583334e77e8ce909e907869
cyc <ycpss91255@gmail.com>
2025-09-07 11:26:32 +0800

    fix: fnm.fish is not written in fish shell format

--- module/config/neovim/fnm_shell_config/fnm.fish
+++ module/config/neovim/fnm_shell_config/fnm.fish
@@ -1,8 +1,9 @@
 # fnm
 set -gx FNM_PATH "$HOME/.local/share/fnm"
+
 if test -d "$FNM_PATH"
-  if ! echo "$PATH" | grep -q "$FNM_PATH"; then
-    set -gx PATH $FNM_PATH $PATH
-  end
-  fnm env --use-on-cd --shell fish | source
+    if not contains $FNM_PATH $PATH
+        set -gx PATH $FNM_PATH $PATH
+    end
+    fnm env --use-on-cd --shell fish | source
 end

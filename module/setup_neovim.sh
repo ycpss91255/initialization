51cb974e6e3e9288f4888c4b7418d61e59f9d083
cyc <ycpss91255@gmail.com>
2025-09-07 14:20:42 +0800

    adjust: fd-find and zoxide use github release

--- module/setup_neovim.sh
+++ module/setup_neovim.sh
@@ -67,13 +67,81 @@ _neovim_dep_pkgs=(
     "python3-pip"
     "python3-venv"
     "ripgrep"
-    "fd-find"
-    "zoxide"
     "yarn"
     "xclip"
 )
 log_info "Install 'Neovim' dependency packages: ${_neovim_dep_pkgs[*]}"
 apt_pkg_manager --install -- "${_neovim_dep_pkgs[@]}"
+
+# fzf
+if [[ -d "${HOME}/.fzf" ]]; then
+    log_info "Backup old fzf installation (${HOME}/.fzf) to ${BACKUP_DIR}/fzf"
+    backup_file "${HOME}/.fzf"
+    rm -rf "${HOME}/.fzf"
+fi
+exec_cmd "git clone --depth 1 \"https://github.com/junegunn/fzf.git\" \"${HOME}/.fzf\" && ${HOME}/.fzf/install --key-bindings --completion --no-update-rc"
+_fzf_bash_conf="[ -f ~/.fzf.bash ] && source ~/.fzf.bash"
+if [[ -f "${HOME}/.bashrc" ]]; then
+    if ! grep -q "${_fzf_bash_conf}" "${HOME}/.bashrc"; then
+        log_info "Add fzf configuration to ${HOME}/.bashrc"
+        exec_cmd "printf '\n%s\n' \"${_fzf_bash_conf}\" >> \"${HOME}/.bashrc\""
+    fi
+fi
+
+# zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+# shellcheck disable=SC2016
+_zoxide_bash_conf='eval "$(zoxide init bash)"'
+if [[ -f "${HOME}/.bashrc" ]]; then
+    if ! grep -q "${_zoxide_bash_conf}" "${HOME}/.bashrc"; then
+        log_info "Add zoxide configuration to ${HOME}/.bashrc"
+        exec_cmd "printf '\n%s\n' \"${_zoxide_bash_conf}\" >> \"${HOME}/.bashrc\""
+    fi
+fi
+# shellcheck disable=SC2016
+_zoxide_zsh_conf='eval "$(zoxide init zsh)"'
+if [[ -f "${HOME}/.zshrc" ]]; then
+    if ! grep -q "${_zoxide_zsh_conf}" "${HOME}/.zshrc"; then
+        log_info "Add zoxide configuration to ${HOME}/.zshrc"
+        exec_cmd "printf '\n%s\n' \"${_zoxide_bash_conf}\" >> \"${HOME}/.zshrc\""
+    fi
+fi
+
+# install fd-find
+log_info "Install fd-find (Github Releases)"
+log_info "Get latest fd-find release version from GitHub"
+_tmp_fdfind=""
+create_temp_file _tmp_fdfind "fdfind" "tar.gz"
+_fdfind_version=""
+_fdfind_repo="sharkdp/fd"
+get_github_pkg_latest_version _fdfind_version "${_fdfind_repo}"
+exec_cmd "curl -fsSL --retry 3 -o \"${_tmp_fdfind}\" \
+    \"https://github.com/${_fdfind_repo}/releases/download/v${_fdfind_version}/fd-v${_fdfind_version}-x86_64-unknown-linux-gnu.tar.gz\""
+
+_fdfind_install_dir="/opt/fdfind"
+_skip_fdfind_install="false"
+if [[ -e "${_fdfind_install_dir}" ]]; then
+    # system fd-find version check
+    if [[ -x "${_fdfind_install_dir}/fd" ]]; then
+        if [[ "$("${_fdfind_install_dir}/fd" --version | awk '{print $2}')" == "${_fdfind_version}" ]]; then
+            log_info "fd-find v${_fdfind_version} already installed to ${_fdfind_install_dir}."
+            _skip_fdfind_install="true"
+        fi
+    fi
+    if [[ "${_skip_fdfind_install}" == "false" ]]; then
+        log_info "Backup old fdfind installation (${_fdfind_install_dir}) to ${BACKUP_DIR}/fdfind"
+        backup_file "${_fdfind_install_dir}"
+        sudo rm -rf "${_fdfind_install_dir}"
+    fi
+fi
+
+if [[ "${_skip_fdfind_install}" == "false" ]]; then
+    log_info "Install fdfind v${_fdfind_version} to ${_fdfind_install_dir}"
+    exec_cmd "sudo mkdir -p -- \"${_fdfind_install_dir}\" && \
+        sudo tar -C \"${_fdfind_install_dir}\" --strip-components=1 -xzf \"${_tmp_fdfind}\" && \
+        sudo ln -sfn \"${_fdfind_install_dir}/fd\" \"/usr/local/bin/fd\""
+fi
+log_info "fdfind v${_fdfind_version} installed to ${_fdfind_install_dir}."
 
 log_info "Install Neovim (Github Releases)"
 log_info "Get latest Neovim release version from GitHub"
@@ -194,6 +262,7 @@ fi
 # NOTE: WARN list
 # codecompanion.nvim
 
+# NOTE: remove lsp_signature.nvim doc tags to avoid error
 # find ~/.local ~/.config/ -type f -path "*/lsp_signature*/doc/tags" -exec rm -f {} \;
 # rm ${HOME}/.local/share/nvim/site/lazy/lsp_signature.nvim/doc/tags
 log_info "Remove lsp_signature.nvim doc tags to avoid error"

36cd2734d31ca49cd38a69d10776ec254db83f37
cyc <ycpss91255@gmail.com>
2025-09-09 11:55:18 +0800

    review: using submodule template

--- module/submodule/batcat.sh
+++ module/submodule/batcat.sh
@@ -19,12 +19,12 @@ if [[ "${MAIN_FILE}" == "true" ]]; then
     unset HAVE_SUDO_ACCESS
 
     # shellcheck disable=SC2155
-    # export DATETIME="$(date +"%Y-%m-%d-%T")"
-    # export BACKUP_DIR="${HOME}/.backup/${DATETIME}"
+    export DATETIME="$(date +"%Y-%m-%d-%T")"
+    export BACKUP_DIR="${HOME}/.backup/${DATETIME}"
 
     SCRIPT_PATH="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-    export FUNCTION_PATH="${SCRIPT_PATH}/function"
-    export CONFIG_PATH="${SCRIPT_PATH}/config"
+    export FUNCTION_PATH="${SCRIPT_PATH}/../function"
+    export CONFIG_PATH="${SCRIPT_PATH}/../config"
 
     :
 fi
@@ -47,30 +47,65 @@ if ! have_sudo_access; then
     fi
 fi
 
-log_info "Install batcat (Github Releases)"
-log_info "Get latest batcat release version from GitHub"
+function install_batcat() {
+    local _github_repo="${1:?"${FUNCNAME[0]}: missing github repo"}"
+    local _run_alias="${2:?"${FUNCNAME[0]}: missing run alias"}"
+    local _strip-components="${3:-0}"
 
-# create temp file
-_tmp_batcat=""
-create_temp_file _tmp_batcat "batcat" "tar.gz"
+    local _pkg_name="${_github_repo##*/}"
+    local _install_dir="/opt/${_pkg_name}"
+    local _tmp_file="" _latest_version="" _install_version="" _repo_url="" _download_file=""
 
-# get latest version from github and download
-_batcat_version=""
-_batcat_repo="sharkdp/bat"
-get_github_pkg_latest_version _batcat_version "${_batcat_repo}"
-exec_cmd "curl -fsSL --retry 3 -o \"${_tmp_batcat}\" \
-    \"https://github.com/${_batcat_repo}/releases/download/v${_batcat_version}/bat-v${_batcat_version}-x86_64-unknown-linux-gnu.tar.gz\""
+    # create temp file
+    create_temp_file _tmp_file "${_pkg_name}" ".tar.gz"
 
-_batcat_install_dir="/opt/batcat"
-if [[ -e "${_batcat_install_dir}" ]]; then
-    log_info "Backup old batcat installation (${_batcat_install_dir}) to ${BACKUP_DIR}/batcat"
-    backup_file "${_batcat_install_dir}"
-    sudo rm -rf "${_batcat_install_dir}"
-fi
+    # get latest version from Github
+    get_github_pkg_latest_version _latest_version "${_github_repo}"
 
-log_info "Install batcat v${_batcat_version} to ${_batcat_install_dir}"
-exec_cmd "sudo mkdir -p -- \"${_batcat_install_dir}\" && \
-    sudo tar -C \"${_batcat_install_dir}\" --strip-components=1 -xzf \"${_tmp_batcat}\" && \
-    sudo ln -sfn \"${_batcat_install_dir}/bat\" \"/usr/local/bin/bat\""
+    # check if already installed
+    if [[ -x "$(command -v "${_run_alias}")" ]];then
+        # NOTE: please check the version command
+        _install_version="$("${_run_alias}" --version | grep -oE '[0-9]+\.[0-9]+(\.[0-9]+)?')"
 
-log_info "batcat v${_batcat_version} installed to ${_batcat_install_dir}."
+        # The latest version is already installed
+        if [[ "${_install_version}" == "${_latest_version}" ]]; then
+            log_info "${_run_alias} v${_latest_version} lready installed to ${_install_dir}."
+            return 0
+        fi
+    fi
+
+    # backup old installation
+    if [[ -d "${_install_dir}" ]]; then
+        log_info "Backup old ${_pkg_name} installation ${_install_dir}) to ${BACKUP_DIR}/${_pkg_name}"
+        backup_file "${_install_dir}"
+        sudo rm -rf "${_install_dir}"
+    fi
+
+    # NOTE: This is example URL, please modify it according to your need
+    _download_file="${_pkg_name}-v${_latest_version}-x86_64-unknown-linux-gnu.tar.gz"
+    _repo_url="https://github.com/${_github_repo}/releases/latest/download/${_download_file}"
+
+    log_info "Download ${_pkg_name} v${_latest_version} from ${_repo_url}"
+    exec_cmd "curl -fsSL --retry 3 -o \"${_tmp_file}\" \"${_repo_url}\""
+
+    # verify download file
+    if ! file "${_tmp_file}" | grep -q "gzip compressed data"; then
+        log_fatal "Downloaded file ${_tmp_file} is not a valid gzip file."
+        return 1
+    fi
+    if ! sudo tar -tzf "${_tmp_file}" &>/dev/null; then
+        log_fatal "Downloaded file ${_tmp_file} is not a valid tar.gz archive."
+        return 1
+    fi
+
+    # install package
+    log_info "Install ${_pkg_name} v${_latest_version} to ${_install_dir}"
+    exec_cmd "sudo mkdir -p -- \"${_install_dir}\" &&\
+        sudo tar -xzf \"${_tmp_file}\" -C \"${_install_dir}\" --strip-components=${_strip-components}"
+    # NOTE: please check the binary name or path
+    exec_cmd "sudo ln -sfn -- \"${_install_dir}/${_pkg_name}\" \"/usr/local/bin/${_run_alias}\""
+
+    log_info "Installed ${_pkg_name} v${_latest_version} to ${_install_dir}, run alias: ${_run_alias}"
+}
+
+install_batcat "sharkdp/bat" "bat" "1"

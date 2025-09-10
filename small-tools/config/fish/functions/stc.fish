74b5ae1ffebe728dc6b55212b80888655f8829ef
cyc <ycpss91255@gmail.com>
2025-09-10 15:51:03 +0800

    del: old script and config

--- small-tools/config/fish/functions/stc.fish
+++
@@ -1,3 +0,0 @@
-function stc --description "source tmux config"
-    tmux source-file "$HOME/.tmux.conf" && echo "source tmux config"
-end
--- small-tools/config/fish/functions/system-update-upgrade.fish
+++
@@ -1,6 +0,0 @@
-function system-update-upgrade --description 'Upgrading the system and removing old packages'
-    sudo apt update &&
-    sudo apt upgrade -y &&
-    sudo apt autoremove -y &&
-    sudo apt autoclean
-end

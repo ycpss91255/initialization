439f919687e4682974700c3fd511dd0ea01c2a7d
cyc <ycpss91255@gmail.com>
2025-09-10 09:34:36 +0800

    adjust: fish config and install step

--- module/config/fish/config.fish
+++ module/config/fish/config.fish
@@ -5,51 +5,12 @@ if status is-interactive
     # end
 
     # if dpkg-query -W -f='${db:Status-Abbrev}\n' -- 'snap' 2>/dev/null | grep -q '^ii'; then
+end
 
-    # Replace vim with neovim
-    if command -q -- "nvim"
-        alias vim="nvim"
-        alias view="nvim -R"
-        set -gx EDITOR "nvim"
-    end
+if not contains $HOME/.local/bin $PATH and test -d $HOME/.local/bin
+    set -Ux PATH $HOME/.local/bin $PATH
+end
 
-    # Replace ls with eza
-    if command -q -- "eza"
-        alias ls="eza"
-        alias ll="eza -l"
-        alias la="eza -la"
-        alias l="eza -l"
-    end
-
-    if command -q -- "xdg-open"
-        alias xopen="xdg-open"
-    end
-
-    if command -q -- "bat"
-        alias cat="bat"
-        alias c="bat"
-    end
-
-    if type -q "fisher"
-        if fisher list | grep -q "/sponge"
-            set -xg sponge_purge_only_on_exit true
-        end
-
-        if fisher list | grep -q "/plugin-pj"
-            set -gx PROJECT_PATHS "$HOME/workspace" $PROJECT_PATHS
-        end
-
-        if fisher list | grep -q "/fish-ssh-agent"
-            set -gx SSH_ENV "$HOME/.ssh/environment"
-        end
-
-        # # TODO: check fzf config
-        # if fisher list | grep -q "/fzf"
-        #     set fzf_preview_dir_cmd eza --all --color=always
-        # end
-    end
-
-    if not contains $HOME/.local/bin $PATH
-        set -Ux PATH $HOME/.local/bin $PATH
-    end
+if not contains $HOME/bin $PATH and test -d $HOME/bin
+    set -Ux PATH $HOME/.local/bin $PATH
 end

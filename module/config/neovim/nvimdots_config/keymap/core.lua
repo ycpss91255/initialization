3b8ba89dc2a6924835a44c560e7ae8222467715d
cyc <ycpss91255@gmail.com>
2025-09-27 14:10:48 +0800

    fix: nvim-tree in lift side

--- module/config/neovim/nvimdots_config/keymap/core.lua
+++ module/config/neovim/nvimdots_config/keymap/core.lua
@@ -1,8 +1,8 @@
 local bind = require("keymap.bind")
 local map_cr = bind.map_cr
 local map_cmd = bind.map_cmd
--- local map_cu = bind.map_cu
--- local map_callback = bind.map_callback
+local map_cu = bind.map_cu
+local map_callback = bind.map_callback
 
 return {
 	["n|<A-[>"] = "",
